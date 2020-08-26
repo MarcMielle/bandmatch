e1f47ad2279c0ccc8acf9737680c2d0c255807c5
MarcMielle <marcmielle96@gmail.com>
2020-08-26 12:25:26 +0200

    Fonts and Bootstrap Colors overwritten

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,3 +1,9 @@
 class Preference < ApplicationRecord
+  INSTRUMENTS  = ["guitare", "voix"]
+  MUSIC_STYLES = ["rock", "jazz"]
+
   belongs_to :entity, polymorphic: true
+
+  validates :instrument, inclusion: { in: Preference::INSTRUMENTS }
+  validates :music_style, inclusion: { in: Preference::MUSIC_STYLES }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
+  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :musicians, only: [ :index, :show, :edit, :update] do
     resources :conversations, only: [ :new, :create ]
   end
+
   resources :conversations, only: [ :index, :show ] do
     resources :messages , only: [ :create ]
   end
+
   resources :bands, only: [:index, :show, :edit, :update] do
     member do
       post :add_musician
