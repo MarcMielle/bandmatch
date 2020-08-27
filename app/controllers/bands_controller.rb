4f1a410d90b160bd5fa0843e204a02c6ef3f6a86
Nicolas Hubert <nicolas.hubert@eleves.ec-nantes.fr>
2020-08-27 11:25:47 +0200

    intermediary commit

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,18 +1,28 @@
 class BandsController < ApplicationController
+
   def index
+    @bands = Band.all
   end
 
   def show
   end
 
   def edit
+    @band = Band.find(params[:id])
   end
 
   def update
+    @band = Band.find(params[:id])
+    @band.update(band_params)
+
+    # no need for app/views/bands/update.html.erb
+    # redirect_to band_path(@band)
   end
 
   def add_musician
   end
+
+  private
 
   def band_params
     params.require(:band).permit(:name, :music_style, :avatar)
