b3fe4c727b87ae7164b5340d70f2b21ba90b03c1
Nicolas Hubert <nicolas.hubert@eleves.ec-nantes.fr>
2020-08-28 15:05:24 +0200

    simple_form

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,13 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
-
-  helper_method :current_user
-  helper_method :logged_in?
-  def current_user
-    User.find_by(id: session[:user_id])
-  end
-  def logged_in?  
-    !current_user.nil?
-  end
-  
   self.abstract_class = true
 end
