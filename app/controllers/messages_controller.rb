290bad25cc249e8914bf0eecbe8f1bdef1f2e630
MarcMielle <marcmielle96@gmail.com>
2020-08-31 16:25:28 +0200

    new controllers and actions for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,15 @@
 class MessagesController < ApplicationController
   def create
-    @conversation = Conversation.find(params[:chatroom_id])
+    @conversation = Conversation.find(params[:conversation_id])
     @message = Message.new(message_params)
     @message.conversation = @conversation
-    @message.user = current_user
+    @message.author = current_user
     if @message.save
       redirect_to conversation_path(@conversation, anchor: "message-#{@message.id}")
     else
       render "conversations/show"
     end
+  end
 
   private
 
