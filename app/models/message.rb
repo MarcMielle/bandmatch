6b0aad2828dd4fb8754015166ca49a772868002c
MarcMielle <marcmielle96@gmail.com>
2020-09-01 12:15:25 +0200

    new messagerie

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :author, class_name: 'User'
-
-  def current_user_author(conversation, current_user)
-    current_user.id == conversation.user1_id ? conversation.user2 : conversation.user1
-  end
 end
