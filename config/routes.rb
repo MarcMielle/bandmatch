8d0b2b81c19a3c3f0ce9509c0db67b2111a705ad
MarcMielle <marcmielle96@gmail.com>
2020-09-02 15:04:41 +0200

    new navbars repaired

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
     resources :messages , only: [ :create ]
   end
 
+  resource :profile
+
   resources :bands, only: [:index, :show, :edit, :update] do
     resources :musicians, only: [] do
       member do
